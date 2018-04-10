b0d051addd9dc48f90499dfece47404b8e735f49
David Elkana <davidelkana@Davids-MacBook-Pro.local>
2018-04-10 19:54:50 +0700

    Initiate Folder Structure

---
+++ Tutorial07Consumer/src/main/java/com/tutorial07/consumer/Tutorial07ConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.tutorial07.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial07ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial07ConsumerApplication.class, args);
+	}
+}
